768db23a59ff0bd07b2a01a627f16c96a5d21753
Panda <44699887+TerriblePanda@users.noreply.github.com>
2019-08-31 14:39:02 +0800

    asm fix

--- src/main/java/org/objectweb/asm/ClassWriter.java
+++ src/main/java/org/objectweb/asm/ClassWriter.java
@@ -149,16 +149,19 @@ public class ClassWriter extends ClassVisitor {
 
   /**
    * The last runtime invisible annotation of this class. The previous ones can be accessed with the
+   * {@link AnnotationWriter#previousAnnotation} field. May be {@literal null}.
    */
   private AnnotationWriter lastRuntimeInvisibleAnnotation;
 
   /**
    * The last runtime visible type annotation of this class. The previous ones can be accessed with
+   * the {@link AnnotationWriter#previousAnnotation} field. May be {@literal null}.
    */
   private AnnotationWriter lastRuntimeVisibleTypeAnnotation;
 
   /**
    * The last runtime invisible type annotation of this class. The previous ones can be accessed
+   * with the {@link AnnotationWriter#previousAnnotation} field. May be {@literal null}.
    */
   private AnnotationWriter lastRuntimeInvisibleTypeAnnotation;
 
@@ -441,7 +444,7 @@ public class ClassWriter extends ClassVisitor {
     MethodWriter methodWriter = firstMethod;
     while (methodWriter != null) {
       ++methodsCount;
-      size += methodWriter.computeMethodInfoSize(version & 0xFFFF, version >>> 16);
+      size += methodWriter.computeMethodInfoSize();
       methodWriter = (MethodWriter) methodWriter.mv;
     }
     // For ease of reference, we use here the same attribute order as in Section 4.7 of the JVMS.
@@ -559,7 +562,7 @@ public class ClassWriter extends ClassVisitor {
     while (methodWriter != null) {
       hasFrames |= methodWriter.hasFrames();
       hasAsmInstructions |= methodWriter.hasAsmInstructions();
-      methodWriter.putMethodInfo(result, version & 0xFFFF, version >>> 16);
+      methodWriter.putMethodInfo(result);
       methodWriter = (MethodWriter) methodWriter.mv;
     }
     // For ease of reference, we use here the same attribute order as in Section 4.7 of the JVMS.
